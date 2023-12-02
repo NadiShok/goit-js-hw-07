fc105bc99ad5362c18b5ffdb09900d3b8c59f039
NadiShok <nadinsh85@gmail.com>
2023-12-02 17:54:49 +0200

    commit1

---
+++ js/task-03.js
@@ -0,0 +1,10 @@
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+    const value = event.currentTarget.value.trim()
+    if (value === "") {
+        output.textContent = "Anonymous";
+    }
+    output.textContent = value;
+});
